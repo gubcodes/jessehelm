28ae4bcb0518eb6ba8995d1d84315f97f35cd00f
gubcodes <sdlkfjsld@sdlkfjsldk.com>
2020-12-30 13:43:37 -0500

    intergrated p5js locally

---
+++ sketch.js
@@ -0,0 +1,33 @@
+let w = window.innerWidth;
+let h = window.innerHeight;
+let sketch = function (p) {
+  p.setup = function () {
+    canvas = p.createCanvas(w, h);
+    p.text('click*', 150, 150)
+  }
+
+  //tracking number of clicks:
+  let times = -1;
+  //array of technologies:
+  let list = ['JAVASCRIPT', 'TYPESCRIPT', 'P5.JS', 'REACT.JS', 'CSS', 'TAILWIND', 'BOOTSTRAP', 'SQL', 'POSTGRES', 'HTML', 'PHOTOSHOP', 'ILLUSTRATOR', 'FIGMA', 'GITHUB', 'HEROKU', 'FIREBASE']
+
+  p.mousePressed = function () {
+    if (times === 15) {
+      times = -1;
+    } else {
+      times = times += 1;
+    }
+    console.log(p.mouseX, p.mouseY);
+    p.text(list[times], p.mouseX, p.mouseY);
+    // prevent default
+    return false;
+  }
+  // window.onresize = function() {
+  //   //assigns new values for width and height variables
+  //   w = window.innerWidth;
+  //   h = window.innerHeight;  
+  //   canvas.p.size(w,h);
+  //   times = -1;
+  // }
+};
+new p5(sketch, window.document.getElementById('p5Projects'));
