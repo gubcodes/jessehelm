a951b0b6945823c83ca52281c41b2c63051f4b72
gubcodes <sdlkfjsld@sdlkfjsldk.com>
2020-12-31 16:25:35 -0500

    intersection observer added

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
   });
 });
+//I MADE THESE section----------
 function chbg(image1, image2, image3) {
   let div1 = document.getElementById('projectBG1');
   let div2 = document.getElementById('projectBG2');
@@ -19,6 +20,43 @@ function chbg(image1, image2, image3) {
   div1.style.opacity = '1';
   div2.style.opacity = '2';
   div3.style.opacity = '3';
-
-  // div1.style.animation = 'spin .5s linear 1';
 }
+//I LIKE TO section---------
+// pick a your text:
+let sentence = 'I ALSO LIKE TO CODE THINGS LIKE THIS. ARTSY STUFF AND BLAH BLAH BLAH...';
+// split it up into an array of letters:
+let letters = sentence.split('');
+// set up the timer:
+const timer = ms => new Promise(res => setTimeout(res, ms));
+// setting whether or not it has been loaded already:
+let loaded = false;
+// async function:
+async function load () {
+  // loop over the length of the array of letters, creating span elements inside the 'testSpan' class, also using the ternary to break the lines where we want:
+  for(i = 0; i < letters.length; i++) {
+      let span = document.createElement("SPAN");
+    span.innerHTML = (i == 20 ? `<br>${letters[i]}` : letters[i]);
+    document.getElementById('animateDiv').appendChild(span).className = 'animateSpan';
+    // set timer length for loop cycle:
+  await timer(30);
+};
+console.log(loaded);
+}
+// call the whole function when scrolled to:
+// load();
+const config = {
+  root: null,
+}
+const visibleDiv = document.querySelector('#animateDiv');
+let isLeaving = false;
+let observer = new IntersectionObserver(function(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting && loaded === false) {
+      loaded = true;
+      load();
+    } else {
+      console.log('not rendered on entry')
+    }
+  });
+}, config);
+observer.observe(visibleDiv);
